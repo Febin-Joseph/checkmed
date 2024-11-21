529467b881a156a69112edfff14f2d0279adec95
FebinDev <febin.joseph@checkmed.in>
2024-11-21 12:49:03 +0530

    logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "Checkmed",
   description: "CheckMed, founded in 2020, is a leading SaaS platform that simplifies cashless insurance services. Serving over 12,000 pin codes, CheckMed provides nationwide doctor coverage, diagnostics, pharmacy access, consultation, and wellness programs, CheckMed offers innovative features like scan & pay, camp module, and pre-post policy process ensuring personalized care and customer satisfaction.",
+  icons: {
+    icon: "/logo.svg"
+  },
 };
 
 export default function RootLayout({ children }) {
